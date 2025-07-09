4e8ec772d7cc4fe377fc245ae173d10bd90b348a
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-07-09 17:00:40 +0800

    Attempted basic Routing

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route('/information')
+def info():
+    return 'Flask is the micro-framework of choice for building Machine Learning API endpoints'
+
+personnel = {
+    "rachel": "Executive Vice President of Managerial Functions",
+    "wallace": "Senior Vice President of Managerial Functions",
+    "rosie": "Staff Vice President of Managerial Functions",
+    "james": "Vice Vice President of Managerial Functions",
+    "henri": "Junior Vice President of Managerial Functions"
+}
+
+@app.route('/profile/<name>')
+def profile(name):
+    if name in personnel:
+        return f"{personnel.get(name)}"
+    else:
+        return "404 error"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
